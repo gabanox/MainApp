15549643e3ea544582555a4a254ccda23b2bb3ef
Gabriel <isc.gabriel.ramirezm@gmail.com>
2013-04-15 22:42:51 -0500

    arvhivos que cambiaron despues del gitignore

--- DrawingToAWindow/AvatarView.h
+++ DrawingToAWindow/AvatarView.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface MyQuartzView : UIView
+@interface AvatarView : UIView
 
 @end
--- DrawingToAWindow/AvatarView.m
+++ DrawingToAWindow/AvatarView.m
@@ -6,26 +6,65 @@
 //  Copyright (c) 2013 badge.me. All rights reserved.
 //
 
-#import "MyQuartzView.h"
+#import "AvatarView.h"
 
-@implementation MyQuartzView
+@implementation AvatarView
 
 - (id)initWithFrame:(CGRect)frame
 {
     self = [super initWithFrame:frame];
     if (self) {
-        // Initialization code
+        
     }
     return self;
 }
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
 - (void)drawRect:(CGRect)rect
 {
-    // Drawing code
+
+    CGImageRef imageRef = [[UIImage imageNamed:@"avatar.jpg"] CGImage];
+   
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    
+    // set the background to black
+    [[UIColor whiteColor] setFill];
+    CGContextFillRect(context, self.bounds);
+    
+    // modify the context coordinates,
+    // UIKit and CoreGraphics are oriented differently
+    CGContextSaveGState(context);
+    CGContextTranslateCTM(context, 0, CGRectGetHeight(rect));
+    CGContextScaleCTM(context, 1, -1);
+    
+    // add clipping path to the context, then execute the clip
+    // this is in effect for all drawing until GState restored
+    CGContextAddEllipseInRect(context, CGRectMake(105,300,100,100));
+
+    CGContextClip(context);
+    
+    // stretch the image to be the size of the view
+    CGContextDrawImage(context, CGRectMake(105,300,100,100), imageRef);
+    CGContextRestoreGState(context);
+        
 }
-*/
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    
+    UITouch *myTouch = [touches anyObject];
+    CGPoint location = [myTouch locationInView:[myTouch view]];
+    NSLog(@"X => %0.0f", location.x);
+    NSLog(@"Y => %0.0f", location.y);
+    
+}
+
+- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {
+    NSLog(@"touchesMoved");
+}
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
+    NSLog(@"touchesEnded");
+}
+- (void)touchesCancelled:(NSSet *)touches withEvent:(UIEvent *)event {
+    NSLog(@"touchesCancelled");
+}
 
 @end
